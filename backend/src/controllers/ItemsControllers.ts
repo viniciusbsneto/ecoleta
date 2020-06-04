01a19226a871c116998479b2fafd8585aef58461
Vinícius Neto <viniciusbsneto@gmail.com>
2020-06-03 21:49:33 -0300

    chore: Add map and items to create point page

--- backend/src/controllers/ItemsControllers.ts
+++ backend/src/controllers/ItemsControllers.ts
@@ -8,7 +8,7 @@ class ItemsController {
     const serializedItems = items.map(item => {
       return {
         id: item.id,
-        title: item.name,
+        title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
       }
     })
